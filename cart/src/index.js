79d1a9175213ff28856ee3be0ae259d2056ffd5f
Axel <axosar@gmail.com>
2021-02-17 10:14:25 -0600

    Cart dependency

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,5 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()}, items in your cart.</div>`;
+const cartText = `<div><h1>You have ${faker.random.number()}, items in your cart.</h1></div>`;
 
 document.querySelector('#dev-cart').innerHTML = cartText;
