63dcbc3db38a34a9af7ce0568c7a52c46052b6fb
Axel <axosar@gmail.com>
2021-02-21 13:46:17 -0600

    Cambios en el codigo

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -8,3 +8,27 @@ for (let i = 0; i < 5; i++){
 }
 
 document.querySelector('#dev-products').innerHTML = products;
+
+const mount = (el) => {
+  let products = '';
+
+  for(let i = 0; i < 5; i++){
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`
+  }
+
+  ReactDOM.render(<App />, el);
+}
+// Context / situation 1
+// Definitely with an id of 'dev-products' will exist
+if(ProcessingInstruction.env.NODE_ENV === 'development'){
+  const el = document.querySelector('#dev-products');
+
+  if(el){
+    mount(el);
+  }
+}
+
+// Context / situation 2
+// No guarantee that an element with an id of 'dev-products' will exist
+export {mount};
