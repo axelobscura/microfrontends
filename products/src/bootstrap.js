eda7331df7a56238b52cb74d6b99de33511b56ac
Axel <axosar@gmail.com>
2021-02-22 12:40:10 -0600

    test

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -4,7 +4,7 @@ let products = '';
 
 for (let i = 0; i < 5; i++){
   const name = faker.commerce.productName();
-  products += `<div><h3>${name}</h3></div>`;
+  products += `<div><h3>${name}</h3>....</div>`;
 }
 
 document.querySelector('#dev-products').innerHTML = products;
